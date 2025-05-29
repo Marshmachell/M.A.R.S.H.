e1adf1bda8f5b80d3a733ca84dec6d7db752606a
Marshmachell <marshmachell@gmail.com>
2025-05-29 20:02:13 +0300

    god damn!!!

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ from cogs.general import BotPing
 from cogs.minecraft import MCServerCommand
 from cogs.fun import AICommand
 
-from flask import Flask
-from flask_scss import Scss
-
-app = Flask(__name__)
-Scss(app)
-
 logger = settings.logging.getLogger("bot")
 
 cogs = [BotPing, MCServerCommand, AICommand]
